58385f47a865a32d8d16a8fec723bcaf1122630a
Даниил <derendervad@gmail.com>
2022-12-13 20:23:29 +0300

    change nullptr

--- Homework 5.1.cpp
+++ Homework 5.1.cpp
@@ -28,134 +28,145 @@ int main()
 
 
 	Figure* t = new Triangle(10, 20, 30, 50, 60, 70);
-	delete t;
+	Figure* t1 = nullptr;
 	try {
-		Figure* t1 = new Triangle(10, 20, 30, 50, 60, 60);
+		t1 = new Triangle(10, 20, 30, 50, 60, 60);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
+	Figure* t2 = nullptr;
 	try {
-		Figure* t2 = new Triangle(10, 20, 0, 50, 60, 70);
+		t2 = new Triangle(10, 20, 0, 50, 60, 70);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	std::cout << std::endl;
+	delete t; delete t1; delete t2;
 
 
 	Figure* rt1 = new Right_triangle(10, 20, 30, 40, 50, 90);
+	Figure* rt = nullptr;
 	try {
-		Figure* rt = new Right_triangle(10, 20, 30, 40, 70, 70);
+		rt = new Right_triangle(10, 20, 30, 40, 70, 70);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete rt1; std::cout << std::endl;
+	delete rt1; delete rt; std::cout << std::endl;
 
 
 	Figure* it = new Isosceles_triangle(10, 20, 10, 50, 80, 50);
+	Figure* it1 = nullptr; Figure* it2 = nullptr;
 	try {
-		Figure* it1 = new Isosceles_triangle(20, 20, 10, 50, 80, 50);
+		it1 = new Isosceles_triangle(20, 20, 10, 50, 80, 50);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	try {
-		Figure* it2 = new Isosceles_triangle(10, 20, 10, 40, 80, 60);
+		it2 = new Isosceles_triangle(10, 20, 10, 40, 80, 60);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete it; std::cout << std::endl;
+	delete it; delete it1; delete it2; std::cout << std::endl;
+
 
 	Figure* et = new Equilateral_triangle(30, 30, 30, 60, 60, 60);
+	Figure* et1 = nullptr; Figure* et2 = nullptr;
 	try {
-		Figure* et1 = new Equilateral_triangle(20, 30, 30, 60, 60, 60);
+		et1 = new Equilateral_triangle(20, 30, 30, 60, 60, 60);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	try {
-		Figure* et2 = new Equilateral_triangle(30, 30, 30, 70, 70, 40);
+		et2 = new Equilateral_triangle(30, 30, 30, 70, 70, 40);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete et; std::cout << std::endl;
+	delete et; delete et1; delete et2; std::cout << std::endl;
 
 	Figure* q = new Quadrangle(10, 20, 30, 40, 80, 80, 100, 100);
+	Figure* q1 = nullptr; Figure* q2 = nullptr;
 	try {
-		Figure* q1 = new Quadrangle(10, 20, 30, 40, 50, 60, 70, 80);
+		q1 = new Quadrangle(10, 20, 30, 40, 50, 60, 70, 80);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	try {
-		Figure* q2 = new Quadrangle(10, 20, 30, 0, 80, 80, 100, 100);
+		q2 = new Quadrangle(10, 20, 30, 0, 80, 80, 100, 100);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete q; std::cout << std::endl;
+	delete q; delete q1; delete q2; std::cout << std::endl;
 
 	Figure* r = new Rectangle_Q(10, 20, 10, 20, 90, 90, 90, 90);
+	Figure* r1 = nullptr; Figure* r2 = nullptr;
 	try {
-		Figure* r1 = new Rectangle_Q(10, 10, 10, 20, 90, 90, 90, 90);
+		r1 = new Rectangle_Q(10, 10, 10, 20, 90, 90, 90, 90);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	try {
-		Figure* r2 = new Rectangle_Q(10, 20, 10, 20, 80, 90, 90, 100);
+		r2 = new Rectangle_Q(10, 20, 10, 20, 80, 90, 90, 100);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete r; std::cout << std::endl;
+	delete r; delete r1; delete r2; std::cout << std::endl;
 
 	Figure* s = new Square(20, 20, 20, 20, 90, 90, 90, 90);
+	Figure* s1 = nullptr; Figure* s2 = nullptr;
 	try {
-		Figure* s1 = new Square(10, 20, 20, 20, 90, 90, 90, 90);
+		s1 = new Square(10, 20, 20, 20, 90, 90, 90, 90);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	try {
-		Figure* s2 = new Square(20, 20, 20, 20, 80, 90, 90, 100);
+		s2 = new Square(20, 20, 20, 20, 80, 90, 90, 100);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete s; std::cout << std::endl;
+	delete s; delete s1; delete s2; std::cout << std::endl;
 
 	Figure* p = new Parallelogram(10, 20, 10, 20, 100, 80, 100, 80);
+	Figure* p1 = nullptr; Figure* p2 = nullptr;
 	try {
-		Figure* p1 = new Parallelogram(10, 10, 10, 20, 100, 80, 100, 80);
+		p1 = new Parallelogram(10, 10, 10, 20, 100, 80, 100, 80);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	try {
-		Figure* p2 = new Parallelogram(10, 20, 10, 20, 100, 100, 100, 60);
+		p2 = new Parallelogram(10, 20, 10, 20, 100, 100, 100, 60);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete p; std::cout << std::endl;
+	delete p; delete p1; delete p2; std::cout << std::endl;
 
 	Figure* rh = new Rhombus(10, 10, 10, 10, 100, 80, 100, 80);
+	Figure* rh2 = nullptr; Figure* rh1 = nullptr;
 	try {
-		Figure* rh1 = new Rhombus(10, 10, 10, 20, 100, 80, 100, 80);
+		rh1 = new Rhombus(10, 10, 10, 20, 100, 80, 100, 80);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
 	try {
-		Figure* rh2 = new Rhombus(10, 10, 10, 10, 100, 100, 100, 60);
+		rh2 = new Rhombus(10, 10, 10, 10, 100, 100, 100, 60);
 	}
 	catch (My_exception& my) {
 		std::cerr << my.what() << std::endl;
 	}
-	delete rh; std::cout << std::endl;
+	delete rh; delete rh1; delete rh2; std::cout << std::endl;
 }
